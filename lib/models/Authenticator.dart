c87855656bf9ce6554074f7fcd19e8c7cea6ec45
Duong Gia Lac <lacduonggia1998@gmail.com>
2019-10-09 09:56:30 +0700

    Adding Firebase Google sign-in for Android app

--- lib/models/Authenticator.dart
+++ lib/models/Authenticator.dart
@@ -1,1 +1,36 @@
-class Authenticator {}
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter/material.dart';
+
+class Authenticator {
+  String signInState;
+  FirebaseUser currentUser;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseUser user;
+
+  Future<FirebaseUser> handleSignIn() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    user = (await _auth.signInWithCredential(credential));
+    return user;
+  }
+
+  getCurrentUser() async {
+    currentUser = await _auth.currentUser();
+    print('Hello ' + currentUser.displayName.toString());
+  }
+
+  void signOut() async {
+    await _googleSignIn.signOut();
+  }
+}
+
+final Authenticator authen = new Authenticator();
