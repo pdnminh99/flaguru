561b02594851c43b02e91015a568135605687a21
cao.nvd@itsstraining.edu.vn <cao.nvd@itsstraining.edu.vn>
2019-10-12 19:02:11 +0700

    commit for delete

--- lib/widgets/menu_card.dart
+++ lib/widgets/menu_card.dart
@@ -1,18 +1,15 @@
-
-import 'dart:ffi';
-
 import 'package:flaguru/widgets/Menu_Icon/menu__icon_icons.dart';
 import 'package:flutter/material.dart';
 
 class Menu extends StatelessWidget {
-
   final onpress;
   String _name_button;
-  IconData icon_main; 
+  IconData icon_main;
   String icon_G;
   BuildContext context_param;
-  Menu(this.onpress,this.icon_main, this._name_button,this.icon_G, this.context_param);
-  
+  Menu(this.onpress, this.icon_main, this._name_button, this.icon_G,
+      this.context_param);
+
   @override
   Widget build(BuildContext context) {
     // return Container(
@@ -27,29 +24,32 @@ class Menu extends StatelessWidget {
       height: 60,
       width: 350,
       child: FlatButton(
-          shape:  RoundedRectangleBorder(borderRadius: new BorderRadius.circular(10.0)),
-          onPressed: () {onpress(context_param);},
+          shape: RoundedRectangleBorder(
+              borderRadius: new BorderRadius.circular(10.0)),
+          onPressed: () {
+            onpress(context_param);
+          },
           padding: EdgeInsets.all(0),
-          color: Colors.white,
-          child:
-           Container(
-             width: double.infinity,
-             height: double.infinity,
-             decoration: BoxDecoration(
-               borderRadius: BorderRadius.circular(10.0),
-               boxShadow: [
-                 BoxShadow(color: Colors.white,
-                 offset: Offset(0,2.0),
-                 blurRadius: 1.0)
-               ]
-             ),
-             child: Row(children: <Widget>[
+          color: Color.fromRGBO(255, 255, 255, 0.7),
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                      color: Color.fromRGBO(250, 250, 250, 0.9),
+                      offset: Offset(0, 2.0),
+                      blurRadius: 1.0)
+                ]),
+            child: Row(children: <Widget>[
               Expanded(
-                    flex: 1,
-                    child: Icon(
-                      icon_main,size: 35,
-                    ),                  
-                    ),
+                flex: 1,
+                child: Icon(
+                  icon_main,
+                  size: 35,
+                ),
+              ),
               Expanded(
                   flex: 4,
                   child: Text(
@@ -58,28 +58,37 @@ class Menu extends StatelessWidget {
                     style: TextStyle(
                       color: Colors.black,
                       fontSize: 35,
-                      fontWeight: FontWeight.w400,
+                      fontWeight: FontWeight.w600,
                     ),
                   )),
-                Expanded(
-                  flex: 1,
-                  child:
-                    icon_G == null ? SizedBox(width: 30,) : 
-                    Container(
-                      width: 30,
-                      height: double.infinity,
-                      decoration: BoxDecoration(border: Border(left: BorderSide(color: Colors.black, width: 2))),
-                      child : Icon(
-                        Menu_Icon.google,
-                        size: 35,
-                        )
-                    )
-                  )
-                  //SizedBox(width: 30,)
-                  //child: Icon(Sword.swords, size: 35,),),
-                  
-          ]),
-           )),
+              Expanded(
+                flex: 1,
+                child: icon_G == null
+                    ? SizedBox(
+                        width: 30,
+                      )
+                    : Container(
+                        width: 30,
+                        height: double.infinity,
+                        decoration: BoxDecoration(
+                            border: Border(
+                                left:
+                                    BorderSide(color: Colors.black, width: 2))),
+                        child: Padding(
+                          padding: EdgeInsets.all(5.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage("./assets/icon/search.png"),
+                                  fit: BoxFit.scaleDown),
+                            ),
+                          ),
+                        )),
+              )
+              //SizedBox(width: 30,)
+              //child: Icon(Sword.swords, size: 35,),),
+            ]),
+          )),
     );
   }
 }
