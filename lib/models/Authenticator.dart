fdc6ea914746f82eddabf1c5d361273f3f91674d
nhatminh <pdn.minh1999@gmail.com>
2019-10-16 06:54:20 +0700

    add closing bracket to class Authentication

--- lib/models/Authenticator.dart
+++ lib/models/Authenticator.dart
@@ -62,53 +62,54 @@ class Authentication {
       print(e);
     }
   }
+}
 
 //class Authenticator {
-  // Future<void> initializeAuthenticator() async {
-  //   // check for current user.
-  //   this._isInit = true;
-  // }
+// Future<void> initializeAuthenticator() async {
+//   // check for current user.
+//   this._isInit = true;
+// }
 
-  // void frontEndDo() {
-  //   var auth = Authenticator();
-  //   await auth.initializeAuthenticator();
-  //   var user = auth.getCurrentUser();
-  //   if (user == null) {
-  //     if (await auth.login() == true) {
-  //       user = auth.getCurrentUser();
-  //     }
-  //   } else {
-  //     // already login
-  //   }
-  // }
+// void frontEndDo() {
+//   var auth = Authenticator();
+//   await auth.initializeAuthenticator();
+//   var user = auth.getCurrentUser();
+//   if (user == null) {
+//     if (await auth.login() == true) {
+//       user = auth.getCurrentUser();
+//     }
+//   } else {
+//     // already login
+//   }
+// }
 
-  // bool _isInit = false;
-  // Map<String, Object> _currentUser;
+// bool _isInit = false;
+// Map<String, Object> _currentUser;
 
-  // Object getCurrentUser() {
-  //   // check
-  //   if (this._isInit == false) throw "You must call initialize ... first";
-  //   if (this._currentUser == null) return null;
-  // }
+// Object getCurrentUser() {
+//   // check
+//   if (this._isInit == false) throw "You must call initialize ... first";
+//   if (this._currentUser == null) return null;
+// }
 
-  // Future<bool> login() async {
-  //   try {
+// Future<bool> login() async {
+//   try {
 
-  //     this._currentUser = await Firebase().login();
-  //     return true;
-  //   } catch(e) {
-  //     return false;
-  //   }
-  // }
+//     this._currentUser = await Firebase().login();
+//     return true;
+//   } catch(e) {
+//     return false;
+//   }
+// }
 
-  // Future<bool> signOut() async {
-  //   try {
+// Future<bool> signOut() async {
+//   try {
 
-  //     return true;
-  //   } catch(e) {
-  //     return false;
-  //   }
-  // }
-  //
-  //
+//     return true;
+//   } catch(e) {
+//     return false;
+//   }
+// }
+//
+//
 //}
