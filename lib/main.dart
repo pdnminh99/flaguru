c9b8e71347f5e828e919dffe0843a7fa3bf8b616
lacduonggia <lac.dg5070@sinhvien.hoasen.edu.vn>
2019-10-23 17:21:03 +0700

    fix some thing

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flaguru/screens/difficulty_screen.dart';
-<<<<<<< HEAD
 import 'package:flaguru/screens/info_screen.dart';
-=======
 import 'package:flaguru/screens/result_screen.dart';
->>>>>>> 7a37298e19257dcf66d46fe25674d08514d7d0be
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'screens/info_screen.dart';
