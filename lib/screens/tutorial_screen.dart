2a68a72d69f73e0227807d57aad5533ae4726cf2
cao.nvd@itsstraining.edu.vn <cao.nvd@itsstraining.edu.vn>
2019-11-19 21:52:58 +0700

    tab view tutorial

--- lib/screens/tutorial_screen.dart
+++ lib/screens/tutorial_screen.dart
@@ -1,6 +1,84 @@
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
-import 'package:path/path.dart' as prefix0;
+
+class Menututorial extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(children: <Widget>[
+        SizedBox(
+          height: 20,
+        ),
+        Align(
+          alignment: Alignment.topCenter,
+          child: Text(
+            'Menu Screen',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: Image.asset(
+              "assets/tutorial_picture/MenuscreenPic.png",
+              width: 250,
+            )),
+            SizedBox(
+              height: 20,
+            ),
+        // RichText(
+        //   text: TextSpan(
+        //     text:'hello', 
+        //     style: TextStyle(fontSize: 20, color: Colors.black)
+        //   ),
+        // )
+      ]),
+    );
+  }
+}
+
+class Difftutorial extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(children: <Widget>[
+        SizedBox(
+          height: 20,
+        ),
+        Align(
+          alignment: Alignment.topCenter,
+          child: Text(
+            'Different Screen',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(8.0),
+          child: Image.asset("assets/tutorial_picture/DiffscreenPic.png",
+          width: 250 ),
+        )
+      ]),
+    );
+  }
+}
+
+class Playtutorial extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class Resulttutorial extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
 
 class TutorialScreen extends StatefulWidget {
   static String routeName = "./tutorial_screen";
@@ -8,102 +86,88 @@ class TutorialScreen extends StatefulWidget {
   _TutorialScreenState createState() => _TutorialScreenState();
 }
 
-class _TutorialScreenState extends State<TutorialScreen> {
+class _TutorialScreenState extends State<TutorialScreen>
+    with SingleTickerProviderStateMixin {
+  static var tutorialPage = <Widget>[
+    new Menututorial(),
+    new Difftutorial(),
+    new Playtutorial(),
+    new Resulttutorial()
+  ];
+  TabController tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    tabController = TabController(vsync: this, length: tutorialPage.length);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color.fromRGBO(220, 220, 220, 1),
-        appBar: AppBar(
-          title: const Text('hello'),
+    double _height = MediaQuery.of(context).size.height;
+    double _width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff019dad),
+        centerTitle: true,
+        title: Text(
+          'Tutorial',
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
         ),
-        body: Container(
-          width: 150,
-          height: double.infinity,
-          padding: EdgeInsets.only(top: 100, left: 10),
-          child: Container(
-            alignment: Alignment.center,
-            width: 130,
-            height: 260,
-            child: ListView.builder(
-              itemBuilder: (BuildContext context, int index) =>
-                  ExpandlistMenu(listoftitle[index]),
-              itemCount: listoftitle.length,
-            ),
-          ),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.popAndPushNamed(context, "/");
+          },
+          color: Colors.white,
+          icon: Icon(Icons.arrow_back_ios),
         ),
       ),
-    );
-  }
-}
-
-List<TitleofDraw> listoftitle = <TitleofDraw>[
-  //Play
-  TitleofDraw(
-    '1. Play',
-  ),
-  TitleofDraw(
-    '1.1 Difficult Screen',
-  ),
-  TitleofDraw(
-    '1.2 Play Screen',
-  ),
-  TitleofDraw(
-    '1.3 Result Screen',
-  ),
-  //Login
-  TitleofDraw(
-    '2. Login',
-  ),
-  //Setting
-  TitleofDraw(
-    '3. Setting',
-  )
-];
-
-class TitleofDraw {
-  String titlename;
-  //Ctor
-  TitleofDraw(this.titlename);
-}
-
-class ExpandlistMenu extends StatelessWidget {
-  final TitleofDraw titleofdraw;
-
-  const ExpandlistMenu(this.titleofdraw);
-
-  Widget _buildExpand(TitleofDraw titleparam) {
-    return Container(
-        margin: EdgeInsets.only(top: 10),
-        width: 30,
-        height: 40,
-        decoration: BoxDecoration(
-          color: Color.fromRGBO(255, 255, 255, 1),
-            borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            boxShadow: <BoxShadow>[
-              BoxShadow(
-                color: Color.fromRGBO(255, 255, 255, 0.1),
-                blurRadius: 2,
-              )
-            ]),
-        child: InkWell(
-          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-          onTap: () => print('hello'),
-          child: Container(
-            padding: EdgeInsets.only(left: 5),
-            alignment: Alignment.centerLeft,
-              child: Text(
-            titleparam.titlename,
-            textAlign: TextAlign.left,
-            style: TextStyle(
-              fontSize: 13,
+      body: Stack(
+        fit: StackFit.expand,
+        overflow: Overflow.clip,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Container(
+                height: _height * 0.922,
+                child: TabBarView(
+                  controller: tabController,
+                  children: tutorialPage,
+                ),
+              ),
+              // TabPageSelector(
+              //   controller: tabController,
+              //   indicatorSize: 13,
+              //   selectedColor: Color(0xff019dad),
+              // ),
+            ],
+          ),
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: IconButton(
+              color: Color(0xff019dad),
+              icon: Icon(Icons.arrow_forward_ios),
+              onPressed: () {
+                print(tabController.index);
+                print(tutorialPage.length);
+                if (tabController.index != tutorialPage.length - 1) {
+                  tabController.animateTo(tabController.index + 1);
+                }
+              },
             ),
-          )),
-        ));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return _buildExpand(this.titleofdraw);
+          )
+        ],
+      ),
+    );
   }
 }
