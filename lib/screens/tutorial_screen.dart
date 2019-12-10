c1df722f994475dfd8ee3f98a52aae75bda2570f
kai618 <vanhieu1601@gmail.com>
2019-12-10 17:23:05 +0700

    refined tutorial screen widget

--- lib/screens/tutorial_screen.dart
+++ lib/screens/tutorial_screen.dart
@@ -1,28 +1,10 @@
-import 'package:flaguru/widgets/tutorial_widget/menuscreen_tutorial.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
+
 import 'package:showcaseview/showcaseview.dart';
+import '../widgets/tutorial_widget/menuscreen_tutorial.dart';
 
-class TutorialScreen extends StatefulWidget {
+class TutorialScreen extends StatelessWidget {
   static String routeName = "/tutorial_screen";
-
-  @override
-  _TutorialScreenState createState() => _TutorialScreenState();
-}
-
-class _TutorialScreenState extends State<TutorialScreen> with SingleTickerProviderStateMixin {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
